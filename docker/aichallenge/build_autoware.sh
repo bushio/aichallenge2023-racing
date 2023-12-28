e579a67742250c1858692cd14ac2a98470e146d5
bushio <kuromady@gmail.com>
2023-12-28 10:55:28 +0900

    dix: add install pygame into dockerfile

--- docker/aichallenge/build_autoware.sh
+++ docker/aichallenge/build_autoware.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cd ./aichallenge_ws
+rosdep update
+rosdep install -y -r -i --from-paths src --ignore-src --rosdistro $ROS_DISTRO
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
 cd ..
