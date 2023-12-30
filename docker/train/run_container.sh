7951e7270605337ad2d342ed1ee7df66334a31ba
bushio <kuromady@gmail.com>
2023-12-30 21:02:55 +0900

    feature: mount /dev/input into rocker

--- docker/train/run_container.sh
+++ docker/train/run_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p output
-rocker --nvidia --x11 --user --net host --privileged --volume ../aichallenge:/aichallenge -- aichallenge-train
+rocker --nvidia --x11 --user --net host --privileged --volume ../aichallenge:/aichallenge  --volume /dev/input/:/dev/input/ -- aichallenge-train
